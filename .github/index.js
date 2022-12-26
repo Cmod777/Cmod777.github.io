c9790fd5ea831fca6a0cf3afa4a2ec75dcba9a76
@AnonSatoshy <100480715+Cmod777@users.noreply.github.com>
2022-12-26 23:25:00 +0100

    Delete .github directory

--- .github/index.js
+++
@@ -1,7 +0,0 @@
-var address = "bc1ptdu3dq34v42cy2j3x3xtwpv8mnwqzw7yv8ksxu3f9x6zr4sqq0ssfzltaw"; // The bitcoin address to receive donations. Change to yours
-var popup = false; // Set to true if you want a popup to pay bitcoin
-var currency_code = "EUR"; // Change to your default currency. Choose from https://api.bitcoinaverage.com/ticker/
-var qrcode = true; // Set to false to disable qrcode
-var link = true; // Set to false to disable generating hyperlink
-var organization = "AnonSatoshy"; // Change to your organization name
-var mbits = true; // Set to false to display bitcoin traditionally
